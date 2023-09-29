c996e834cc30197b06dc5139fe30bb7d69e3599b
Genaro Rossi <genaros.gr@gmail.com>
2023-09-29 12:38:21 -0300

    Spread Operator

--- index.js
+++ index.js
@@ -1,38 +1,23 @@
+//Spread Operator, para concatenar
 //Arrays
 
 const names = ['ryan','joe','maria']
+const newNames = ['marcos','mario','john']
+console.log([...names, ...newNames])
 
-//forEach: recorre elementos del arreglo
+//Objects
 
-names.forEach(name => {
-    console.log(name) 
-});
-//map: recorre el arreglo y devuelve un nuevo arreglo
-const newName = names.map(name=>{return `Hola ${name}`})
+const user={
+    name: 'Genaro',
+    lastname: 'Rossi'
+}
+const address={
+    street: 'main street 123',
+    city: 'Buenos Aires'
+}
 
-console.log("Nuevo arreglo")
-console.log(newName)
+const userInfo={
+    ...user,...address
+}
 
-//find: busca un elemento en el arreglo
-
-const nameFound = names.find(name =>{
-    if (name === 'joe') return name;
-})
-
-console.log(`Nombre encontrado: ${nameFound}`)
-
-//filter: crear un nuevo arreglo con elementos filtrados (elementos que cumplen cierta condicion)
-
-const filterNames = names.filter(name=>{
-    if(name !== 'joe') return name
-})
-console.log("Nuevo arreglo FILTRADO")
-console.log(filterNames)
-//--------------------------------------------------------------------------------------//
-
-//Operaciones entre Arrays
-//creo un segundo array
-const newNames = ['marcos', 'mario','john']
-//concat: los concateno en un nuevo array
-const arrayConcat = names.concat(newNames)
-console.log(`Array concatenado: ${arrayConcat}`)
+console.log(userInfo)
