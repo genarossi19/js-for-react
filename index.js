be923210276198fee713c80ce15d4ade857bfa68
Genaro Rossi <genaros.gr@gmail.com>
2023-09-29 12:29:14 -0300

    arrays

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-//Return en funciones y String Literals
-const button = document.createElement('button')
-button.innerText='Clickeame'
-const isAuthorized = false;
-button.addEventListener('click',()=>{
-    if(isAuthorized) return alert('Esta autorizado')
-    alert('No esta autorizado')
+//Arrays
+
+const names = ['ryan','joe','maria']
+
+//forEach: recorre elementos del arreglo
+
+names.forEach(name => {
+    console.log(name) 
+});
+//map: recorre el arreglo y devuelve un nuevo arreglo
+const newName = names.map(name=>{return `Hola ${name}`})
+
+console.log("Nuevo arreglo")
+console.log(newName)
+
+//find: busca un elemento en el arreglo
+
+const nameFound = names.find(name =>{
+    if (name === 'joe') return name;
 })
-button.style = `background: ${isAuthorized ? 'blue' : 'red'};`
-document.body.append(button)
+
+console.log(`Nombre encontrado: ${nameFound}`)
+
+//filter: crear un nuevo arreglo con elementos filtrados (elementos que cumplen cierta condicion)
+
+const filterNames = names.filter(name=>{
+    if(name !== 'joe') return name
+})
+console.log("Nuevo arreglo FILTRADO")
+console.log(filterNames)
+//--------------------------------------------------------------------------------------//
+
+//Operaciones entre Arrays
+//creo un segundo array
+const newNames = ['marcos', 'mario','john']
+//concat: los concateno en un nuevo array
+const arrayConcat = names.concat(newNames)
+console.log(`Array concatenado: ${arrayConcat}`)
